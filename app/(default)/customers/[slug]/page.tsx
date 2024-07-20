ec504fda08b527f8979485efc4e6a707181b3711
abadifaizal <abadifaizal@gmail.com>
2024-07-20 20:39:07 +0900

    add dynamic customer post from contentful

--- app/(default)/customers/[slug]/page.tsx
+++ app/(default)/customers/[slug]/page.tsx
@@ -12,7 +12,7 @@ import RelatedPosts from './related-posts'
 import { getContentForCustomerPost } from '@/content/queries'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 
-export default async function CustomerSingle({ params }) {
+export default async function CustomerSingle({ params }: { params: { slug: string } }) {
   const data = await getContentForCustomerPost(params.slug);
   const content = data.customerPostCollection.items[0];
 
@@ -62,7 +62,10 @@ export default async function CustomerSingle({ params }) {
 
                   {/* Post content */}
                   <div className="prose max-w-none text-slate-400 prose-headings:text-slate-50 prose-h2:text-xl prose-h2:mt-8 prose-h2:mb-4 prose-p:leading-relaxed prose-a:text-purple-500 prose-a:no-underline hover:prose-a:underline prose-strong:text-slate-50 prose-strong:font-medium prose-blockquote:pl-5 prose-blockquote:xl:-ml-5 prose-blockquote:border-l-2 prose-blockquote:border-purple-500 prose-blockquote:font-medium prose-blockquote:text-slate-300 prose-blockquote:italic">
-                    {documentToReactComponents(content.body.json)}
+                    {documentToReactComponents(
+                      // @ts-ignore
+                      content.body.json
+                    )}
                   </div>
                 </article>
 
